cd2d36fcdb5d381bce826bab6d201d8d85586e4d
Pav_lov_sky <infopavlovskiy@gmail.com>
2021-08-09 02:06:58 +0300

    commit1

---
+++ homework-07/js/task-08.js
@@ -0,0 +1,29 @@
+const controls = {
+    inputNumber: document.querySelector('[type="number"]'),
+    buttonRender: document.querySelector('[data-action="render"]'),
+    buttonDestroy: document.querySelector('[data-action="destroy"]'),
+}
+const boxes = document.querySelector('#boxes');
+
+function getAmount(event) {
+    const amount = Number(event.currentTarget.value);
+    createBoxes(amount)
+}
+function random() {
+  return Math.floor(Math.random() * 256);
+}
+function createBoxes(amount){
+   const startlSize = 30;
+    for (let i = 0; i < amount; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.style.backgroundColor = "rgba( ${random()} , ${random()} , ${random()} )";
+         boxes.insertAdjacentHTML('beforeend', newDiv);
+     }
+}
+
+function destroyBoxes() {
+    boxes.removeChild()
+}
+controls.inputNumber.addEventListener('input',getAmount )
+controls.buttonRender.addEventListener('click',createBoxes )
+controls.buttonDestroy.addEventListener('click', destroyBoxes)
