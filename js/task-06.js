55cb2ac596e560ae5dd6580cc45cb584aa2eb07b
Pav_lov_sky <infopavlovskiy@gmail.com>
2021-08-14 13:42:55 +0300

    commit-js7

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,24 @@
-const inputValidation = document.querySelector("#validation-input");
-const dataLength = Number(inputValidation.dataset.length);
-function checkDataLength(event) {
-    if (event.currentTarget.value.length === dataLength) {
-        event.currentTarget.classList.add("valid");
-    } else {
-         event.currentTarget.classList.add("invalid");
-    }
-}
-inputValidation.addEventListener('blur', checkDataLength);
+const inputEl = document.querySelector("#validation-input");
+console.log(inputEl);
+
+inputEl.addEventListener("blur", () => {
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+});
+console.log(inputEl);
+
+// const inputValidation = document.querySelector("#validation-input");
+// const dataLength = Number(inputValidation.dataset.length);
+// function checkDataLength(event) {
+//     if (event.currentTarget.value.length === dataLength) {
+//         event.currentTarget.classList.add("valid");
+//     } else {
+//          event.currentTarget.classList.add("invalid");
+//     }
+// }
+// inputValidation.addEventListener('blur', checkDataLength);
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,20 @@
-const inputSizeControl = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-function changeFontSize(event) {
-    const textSize = event.target.value;
-    text.style.fontSize = `${textSize}%`;
-}
-inputSizeControl.addEventListener('input', changeFontSize);
+/* - First variant - */
+let controlFontSize = document.getElementById("font-size-control");
+
+let textEl = document.getElementById("text");
+
+controlFontSize.oninput = function () {
+  textEl.style.fontSize = controlFontSize.value + "px";
+};
+
+console.log(textEl);
+console.log(controlFontSize);
+
+/* - Second variant - */
+// const inputSizeControl = document.querySelector('#font-size-control');
+// const text = document.querySelector('#text');
+// function changeFontSize(event) {
+//     const textSize = event.target.value;
+//     text.style.fontSize = `${textSize}%`;
+// }
+// inputSizeControl.addEventListener('input', changeFontSize);
